e09f6e975a98d7b4f52ffb8423ea3255bf453767
Thomas Werthenbach <tak.werthenbach@gmail.com>
2023-03-07 16:31:00 +0100

    Aggregate attack rate script

--- experiments/aggregate_accuracies.py
+++ experiments/aggregate_accuracies.py
@@ -37,8 +37,32 @@ class AccuracyStatisticsParser(StatisticsParser):
             for avg in average:
                 sum_id_file.write(f"{avg}\n")
 
+    def aggregate_peer_attack_rates(self):
+
+        filename = os.path.join(os.path.dirname(__file__), sys.argv[2])
+        with open(filename) as f:
+            settings = json.loads("".join([x.strip() for x in f.readlines()]))
+
+        total_attack_rates = list()
+
+        for i in range(settings['peers_per_host']):
+            for _, filename, _ in self.yield_files(f'attack_rate_{i}.csv'):
+                total_attack_rates.append(list(pd.read_csv(filename)[f'attack_rate_{i}']))
+
+        average = list()
+        for i in range(min(list(map(lambda x: len(x), total_attack_rates)))):
+            s = 0
+            for j in range(100):
+                s += total_attack_rates[j][i]
+            average.append(s / 100)
+
+        with open("attack_rate.csv", "w") as sum_id_file:
+            for avg in average:
+                sum_id_file.write(f"{avg}\n")
+
     def run(self):
         self.aggregate_peer_accuracies()
+        self.aggregate_peer_attack_rates()
 
 
 # cd to the output directory
